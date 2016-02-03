3b90b988bf2b4be9f5222b2a80167c11c7931dd7
彭修林 <pengxiulin@jd.com>
2016-02-03 13:00:02 +0800

    完成代理的基本功能，还没有做异常处理

--- index.js
+++ index.js
@@ -6,18 +6,60 @@ http.createServer(function(request, response){
     let HOST = request.headers.host;
     let URL = request.url;
     let TYPE = getType(HOST, URL);
-    console.log(TYPE);
-    response.writeHead(200, {});
-    response.end("Hey Oh!\n");
+    if(TYPE == "PROXY"){
+        // 1. 解析请求内容，构建远端请求对象
+        var opt = {
+            host : 'localhost',
+            port : '80',
+            method : request.method,
+            path : '/',
+            headers : request.headers
+        };
+        var hostArray = HOST.split(":");
+        opt.host = hostArray[0];
+        if(hostArray.length>1){
+            opt.port = hostArray[1];
+        }
+        var urlArray = request.url.split('/');
+        if(urlArray.length>2){
+            for (var i = 3; i < urlArray.length; i++) {
+                opt.path += "/" + urlArray[i];
+            };
+        }else{
+            opt.path = "/";
+        }
+
+        // 2. 发起请求，将内容转发给客户端
+        var serverReq = http.request(opt, function(serverRes) {
+            response.writeHead(serverRes.statusCode, serverRes.headers);
+            serverRes.on('data',function(d){
+                response.write(d);
+            }).on('end', function(){
+                response.end();
+            });
+
+        }).on('error', function(e) {
+            console.log("Got error: " + e.message);
+        });
+        serverReq.end();
+    }else if(TYPE == "STATIC"){
+        response.writeHead(200, {});
+        response.write("设置命令：networksetup -setwebproxy USB\\ Ethernet 127.0.0.1 8000\n");
+        response.end("关闭代理设置：networksetup -setwebproxystate USB\\ Ethernet off\n");
+    }else if(TYPE == "DIRECTION"){
+        response.writeHead(200, {});
+        response.end("代理设置命令\n");
+    }
 }).listen(PROXYPORT);
+
 /**
  * 获取类型
- * @param  {[string]} HOST    [传入的HOST名称]
- * @param  {[string]} URL     [传入的URL地址]
- * @return {[string]}         获取类型： STATIC, PROXY, DIRECTION
+ * @param  {[string]} HOSTINFO  [传入的HOST名称]
+ * @param  {[string]} URL       [传入的URL地址，用于区分指令和静态页面请求]
+ * @return {[string]}           获取类型： STATIC, PROXY, DIRECTION
  * STATIC: 静态资源请求
  * PROXY: 代理请求
- * DIRECTION: 代理设置请求
+ * DIRECTION: 代理信息查看和设置请求
  */
 var getType = function(HOSTINFO, URL){
     var type = "PROXY";
@@ -31,7 +73,7 @@ var getType = function(HOSTINFO, URL){
        && port==PROXYPORT){
         // 若为本地hostname和本地代理服务器监听的端口号
         // 则类型为STATIC
-        // TODO ： 需要监听一些命令
+        // TODO ： 需要根据URL来判断是Web资源请求还是代理查询、处理指令
         type = "STATIC";
     }
     return type;
